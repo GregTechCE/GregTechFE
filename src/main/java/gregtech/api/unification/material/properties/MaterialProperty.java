9e0cb438c957334c14ae8c056529327a7a661167
LAGIdiot <bordel.muj@gmail.com>
2021-06-25 20:29:40 +0200

    Add Builder to MaterialProperty

--- src/main/java/gregtech/api/unification/material/properties/MaterialProperty.java
+++ src/main/java/gregtech/api/unification/material/properties/MaterialProperty.java
@@ -1,11 +1,47 @@
 package gregtech.api.unification.material.properties;
 
+import gregtech.api.unification.material.flags.MaterialFlag;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MaterialProperty<T> {
     private final String name;
-    private final T propertyValue;
+    private final T propertyValueType;
+    private final List<MaterialFlag> requiredFlags = new ArrayList<>();
+    private final List<MaterialProperty<?>> requiredProperties = new ArrayList<>();
 
-    public MaterialProperty(String name, T propertyValue){
+    private MaterialProperty(String name, T propertyValueType, List<MaterialProperty<?>> requiredProperties, List<MaterialFlag> requiredFlags) {
         this.name = name;
-        this.propertyValue = propertyValue;
+        this.propertyValueType = propertyValueType;
+        this.requiredFlags.addAll(requiredFlags);
+        this.requiredProperties.addAll(requiredProperties);
+    }
+
+    public static class Builder<T> {
+        private final String name;
+        private final T propertyType;
+
+        private final List<MaterialFlag> requiredFlags = new ArrayList<>();
+        private final List<MaterialProperty<?>> requiredProperties = new ArrayList<>();
+
+        public Builder(String name, T propertyType) {
+            this.name = name;
+            this.propertyType = propertyType;
+        }
+
+        public MaterialProperty.Builder<T> requires(MaterialProperty<?> property) {
+            this.requiredProperties.add(property);
+            return this;
+        }
+
+        public MaterialProperty.Builder<T> requires(MaterialFlag materialFlag) {
+            this.requiredFlags.add(materialFlag);
+            return this;
+        }
+
+        public MaterialProperty<T> build() {
+            return new MaterialProperty<>(name, propertyType, requiredProperties, requiredFlags);
+        }
     }
 }
