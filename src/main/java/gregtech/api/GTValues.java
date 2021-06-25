b301f9fc14b6388d8d7670e346795b485350dc00
LAGIdiot <bordel.muj@gmail.com>
2021-06-25 19:18:00 +0200

    Returned M back to GTValues as it is used for material amounts

--- src/main/java/gregtech/api/GTValues.java
+++ src/main/java/gregtech/api/GTValues.java
@@ -1,15 +1,22 @@
 package gregtech.api;
 
-import net.minecraftforge.fml.common.Loader;
-import net.minecraftforge.oredict.OreDictionary;
-
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-
 /**
  * Made for static imports, this Class is just a Helper.
  */
 public class GTValues {
+
+    /**
+     * <p/>
+     * This is worth exactly one normal Item.
+     * This Constant can be divided by many commonly used Numbers such as
+     * 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 24, ... 64 or 81
+     * without loosing precision and is for that reason used as Unit of Amount.
+     * But it is also small enough to be multiplied with larger Numbers.
+     * <p/>
+     * This is used to determine the amount of Material contained inside a prefixed Ore.
+     * For example Nugget = M / 9 as it contains out of 1/9 of an Ingot.
+     */
+    public static final long M = 3628800;
 
     /**
      * The Voltage Tiers. Use this Array instead of the old named Voltage Variables
