03ef368180dcb15b1ea072289f168ca1abe51d1f
LAGIdiot <bordel.muj@gmail.com>
2021-06-25 19:25:29 +0200

    Removed excessive brackets

--- src/main/java/gregtech/api/unification/material/flags/MaterialFlags.java
+++ src/main/java/gregtech/api/unification/material/flags/MaterialFlags.java
@@ -2,8 +2,8 @@ package gregtech.api.unification.material.flags;
 
 public class MaterialFlags {
 
-    public static final MaterialFlag GENERATE_PLATE = (new MaterialFlag.Builder("GENERATE_PLATE").build());
-    public static final MaterialFlag GENERATE_FOIL = (new MaterialFlag.Builder("GENERATE_FOIL").requires(GENERATE_PLATE).build());
+    public static final MaterialFlag GENERATE_PLATE = new MaterialFlag.Builder("GENERATE_PLATE").build();
+    public static final MaterialFlag GENERATE_FOIL = new MaterialFlag.Builder("GENERATE_FOIL").requires(GENERATE_PLATE).build();
 
 
     private MaterialFlags() {
