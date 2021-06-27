0a2b95fc5455473e185564a5dfb38b87e8d6e68a
LAGIdiot <bordel.muj@gmail.com>
2021-06-27 12:46:18 +0200

    Convert Element to new system Add Chemical property

---
+++ src/main/java/gregtech/api/unification/Elements.java
@@ -0,0 +1,12 @@
+package gregtech.api.unification;
+
+public class Elements {
+
+    public static final Element H = new Element.Builder("Hydrogen", "H").build();
+
+    public static final Element Al = new Element.Builder("Aluminium", "Al").build();
+
+    private Elements(){
+
+    }
+}
